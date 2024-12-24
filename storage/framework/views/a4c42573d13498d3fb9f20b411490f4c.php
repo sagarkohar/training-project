77ded3383013a6a03c85e5d0416cf8034a99ed8d
sagarkohar <shivsagarkohar32@gmail.com>
2024-12-24 10:28:48 +0530

    added

---
+++ storage/framework/views/a4c42573d13498d3fb9f20b411490f4c.php
@@ -0,0 +1,12 @@
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      HRM
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+      <li><a class="dropdown-item" href="/learning-home">Learning App</a></li>
+      <li><a class="dropdown-item" href="/attendance-home">Attendance</a></li>
+
+
+      
+    </ul>
+  </div><?php /**PATH C:\xampp\htdocs\training-project\resources\views/sidebar.blade.php ENDPATH**/ ?>
